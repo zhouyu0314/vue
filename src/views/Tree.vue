1b835ef1498c6eef8e73884249806810319a1472
zy <181332969@qq.com>
2020-10-22 22:51:18 +0800

    1022

--- src/views/Tree.vue
+++ src/views/Tree.vue
@@ -1,41 +1,18 @@
 <template>
-    <div class="tranferbox">
-        <div class="conbox">
-            <div class="titbox">
-                <el-input size="mini" placeholder="搜索成员、部门" suffix-icon="el-icon-search" v-model="filterText"></el-input>
-            </div>
-            <div class="wordbox">
-                <el-tree
-                        @check="getData"
-                        show-checkbox
-                        class="filter-tree"
-                        node-key="id"
-                        :data="data"
-                        :props="defaultProps"
-                        :filter-node-method="filterNode"
-                        ref="tree"
-                ></el-tree>
-            </div>
-        </div>
-        &emsp;
-        <i class="el-icon-arrow-right"></i>&emsp;
-        <div class="conbox">
-            <div class="titbox">
-                <h2>已选择用户（{{keyarr.length}}）</h2>
-            </div>
-            <div class="wordbox">
-                <ul>
-                    <li v-for="(item,index) in keyarr" :key="index">
-                        <div class="inli">
+    <div>
 
-                            <i class="el-icon-s-custom"></i>
-                            <span>{{item.label}}</span>
-                            <i class="el-icon-close" @click="removeData(item)"></i>
-                        </div>
-                    </li>
-                </ul>
-            </div>
-        </div>
+        <el-button type="primary" @click="handleOpen">打开</el-button>
+        <el-dialog width="50%" :visible.sync="dialogTableVisible" title="TREE" @open="show">
+            <el-tree node-key="id" :data="param" :props="defaultProps" :filter-node-method="filterNode" ref="tree"
+                     :highlight-current="true" :render-after-expand="false">
+                    <span slot-scope="{node,param}" class="myspan">
+                        <span v-text="node.label" ></span>
+                        <span style="position: absolute;right: 0" class="myspan">
+                            <el-button type="primary" size="mini" v-show="node.isLeaf">删除</el-button>
+                        </span>
+                    </span>
+            </el-tree>
+        </el-dialog>
     </div>
 </template>
 <script>
@@ -43,92 +20,57 @@
         name: "Tree",
         data() {
             return {
-                input3: "",
-                checkList: [],
-                keyarr:[],
                 filterText: "",
-                data:[{
-                    id:1,
-                    label: '一级 1',
-                    children: [{
-                        label: '二级 1-1',
-                        id:12,
-                        children: [{
-                            id:13,
-                            label: '三级 1-1-1'
-                        }]
-                    }]
-                }, {
-                    id:2,
-                    label: '一级 2',
-                    children: [{
-                        label: '二级 2-1',
-                        id:21,
-                        children: [{
-                            id:22,
-                            label: '三级 2-1-1'
-                        }]
-                    }, {
-                        id:23,
-                        label: '二级 2-2',
-                        children: [{
-                            id:24,
-                            label: '三级 2-2-1'
-                        }]
-                    }]
-                }, {
-                    label: '一级 3',
-                    id:3,
-                }],
+                dialogTableVisible: false,
+                param: [
+                    {
+                        id: "1",
+                        label: "政府相关",
+                        children: [{id: "12", label: "好好学习，天天向上！"}]
+                    },
+                    {
+                        id: "2",
+                        label: "银行相关",
+                        children: [{id: "21", label: "好好学习，天天向上！"},
+                            {id: "23", label: "好好学习，天天向上！"}]
+                    },
+                    {
+                        id: "3",
+                        label: "企业相关",
+                        children: [{id: "21", label: "好好学习，天天向上！"},
+                            {id: "23", label: "好好学习，天天向上！"}]
+                    }
+                ],
                 defaultProps: {
                     children: "children",
-                    label: "label",
+                    label: "label"
                 },
             };
         },
-        watch: {
-            filterText(val) {
-                this.$refs.tree.filter(val);
-            }
-        },
         methods: {
+            show(){
+                setTimeout(()=>{
+                    console.log("开");
+                    let elementsByClassName = document.getElementsByClassName("el-tree-node__content");
+                    //debugger
+                    for (let i = 0; i < elementsByClassName.length; i++) {
+                        //let parentNode = elementsByClassName[i].parentNode;
+                        elementsByClassName[i].style = elementsByClassName[i].style.cssText + " margin: 5px 0; "
+                    }
+                },100)
+
+            },
+            handleOpen() {
+                this.dialogTableVisible = true;
+
+
+            },
             //关键词搜索
             filterNode(value, data) {
                 if (!value) return true;
                 return data.label.indexOf(value) !== -1;
-            },
-            getData() {
-                this.keyarr=[]
-                this.checkList = this.$refs.tree.getCheckedNodes();
-                console.log(this.checkList)
-                if(this.checkList.length!=0){
-                    for (var i = 0; i < this.checkList.length; i++) {
-                        if (!this.checkList[i].children) {
-                            this.keyarr.push(this.checkList[i])
-                            console.log(this.checkList[i])
-                        }
-                    }
-
-                }else{
-                    this.keyarr=[]
-                }
-            },
-            setCheckedNodes(arr) {
-                this.$refs.tree.setCheckedNodes(arr);
-            },
-            removeData(data) {
-                let checklist = this.keyarr;
-                console.log(this.keyarr)
-                for (var i = 0; i < checklist.length; i++) {
-                    if (checklist[i].label === data.label) {
-                        checklist.splice(i, 1);
-                    }
-                }
-                console.log(this.keyarr)
-                this.keyarr = checklist;
-                this.setCheckedNodes(this.keyarr);
-            },
-        },
+            }
+        }
     };
 </script>
 
