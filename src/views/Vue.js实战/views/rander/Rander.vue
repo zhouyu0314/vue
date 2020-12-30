0ea1ba23875039be7555a38c3faf145284d0513a
zhouyu0314 <181332969@qq.com>
2020-12-30 11:03:50 +0800

    bn

--- src/views/Vue.js实战/views/rander/Rander.vue
+++ src/views/Vue.js实战/views/rander/Rander.vue
@@ -1,33 +1,17 @@
 <template>
     <div>
-        <el-col :span="6">
-            <el-input clearable size="mini" v-model="input"></el-input>
-<!--            &lt;!&ndash;代码冗长&ndash;&gt;-->
-<!--            <template v-if="input == 1">-->
-<!--                <h1>标签{{input}}</h1>-->
-<!--            </template>-->
-<!--            <template v-else-if="input == 2">-->
-<!--                <h2>标签{{input}}</h2>-->
-<!--            </template>-->
-<!--            <template v-else-if="input == 3">-->
-<!--                <h3>标签{{input}}</h3>-->
-<!--            </template>-->
-<!--            <template v-else-if="input == 4">-->
-<!--                <h4>标签{{input}}</h4>-->
-<!--            </template>-->
-<!--            <template v-else-if="input == 5">-->
-<!--                <h5>标签{{input}}</h5>-->
-<!--            </template>-->
-<!--            <template  v-else-if="input == 6"><h6>标签{{input}}</h6></template>-->
 
-
-        </el-col>
+        <render-sub1></render-sub1>
     </div>
 </template>
 
 <script>
+    import RenderSub1 from "./components/RenderSub1";
     export default {
         name: "Rander",
+        components:{
+            'render-sub1':RenderSub1,
+        },
         data(){
             return{
                 input:'',
--- src/views/Vue.js实战/views/rander/rademe.js
+++ src/views/Vue.js实战/views/rander/rademe.js
@@ -1,29 +1,29 @@
 // @returns {VNode}
 createElement(
-  // {String | Object | Function}
-  // 一个 HTML 标签字符串，组件选项对象，或者
-  // 解析上述任何一种的一个 async 异步函数。必需参数。
-  'div',
+    // {String | Object | Function}
+    // 一个 HTML 标签字符串，组件选项对象，或者
+    // 解析上述任何一种的一个 async 异步函数，必要参数。
+    'div',
 
-  // {Object}
-  // 一个包含模板相关属性的数据对象
-  // 你可以在 template 中使用这些特性。可选参数。
-  {
-    // (详情见下一节)
-  },
+    // {Object}
+    // 一个包含模板相关属性的数据对象
+    // 这样，您可以在 template 中使用这些属性。可选参数。
+    {
+      // (详情见下一节)
+    },
 
-  // {String | Array}
-  // 子虚拟节点 (VNodes)，由 `createElement()` 构建而成，
-  // 也可以使用字符串来生成“文本虚拟节点”。可选参数。
-  [
-    '创建h1标签',
-    createElement('h1', 'Render函数'),
-    createElement(MyComponent, {
-      props: {
-        someProp: 'foobar'
-      }
-    })
-  ]
+    // {String | Array}
+    // 子节点 (VNodes)，由 `createElement()` 构建而成，
+    // 或使用字符串来生成“文本节点”。可选参数。
+    [
+      '先写一些文字',
+      createElement('h1', '一则头条'),
+      createElement(MyComponent, {
+        props: {
+          someProp: 'foobar'
+        }
+      })
+    ]
 )
-
+//就是说createElement（params1，params2，params3）接受三个参数，每个参数的类型官方介绍已经说明
 //https://www.cnblogs.com/yubin-/p/11543734.html
