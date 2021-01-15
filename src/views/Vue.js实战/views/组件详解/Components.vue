e43ca22ed7f75d79fc610b444b5b84715badea87
zhouyu0314 <181332969@qq.com>
2021-01-15 15:08:32 +0800

    子组件修改父组件的值

--- src/views/Vue.js实战/views/组件详解/Components.vue
+++ src/views/Vue.js实战/views/组件详解/Components.vue
@@ -1,19 +1,26 @@
 <template>
     <div>
         <add :number="number" @addNum="addNumListen" @click.native="handleAdd"></add>
+        <el-divider></el-divider>
+        <!--:updateMsg.sync 子组件修改此数组可通过emit同步到父组件-->
+        <update :updateMsg.sync="updateMsg"></update>
+        <el-button @click="updateBtn" type="primary" size="mini">打印updateMsg</el-button>
     </div>
 </template>
 
 <script>
     import Add from "./components/Add";
+    import Update from "./components/Update";
 
     export default {
         name: "Components",
         components: {
-            'add': Add
+            'add': Add,
+            'update':Update
         }, data() {
             return {
                 number:0,
+                updateMsg:'h2'
             }
         },methods:{
             addNumListen(item){
@@ -21,6 +28,9 @@
             },
             handleAdd(){//监听的是该组件的根元素
                 this.$message.success('监听一个原生事件');
+            },
+            updateBtn(){
+            this.$message.success(this.updateMsg)    ;
             }
         }
     }
---
+++ src/views/Vue.js实战/views/组件详解/components/Update.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <h2>{{updateMsg}}</h2>
+        <el-button @click="subUpdateBtn" type="primary" size="mini">子组件修改Msg</el-button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Update",
+        props:{
+            updateMsg:{
+                type:String,
+                default:()=> null
+            }
+        },
+        methods:{
+            subUpdateBtn(){
+                this.updateMsg = 'h3';
+                this.$emit('update:updateMsg',this.updateMsg);//update:updateMsg名字是预先约定好的不可随便更改
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
